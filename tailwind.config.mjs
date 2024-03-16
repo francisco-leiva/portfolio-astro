46585da5ea827793668eec6dd76fb9c0a875f3a9
francisco-leiva <leivafrancisco631@gmail.com>
2024-03-16 16:13:31 -0300

    style: add animation at the intersection of cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: {
+        reveal: 'reveal 1s both',
+      },
+      keyframes: {
+        reveal: {
+          from: { opacity: 0, transform: 'translateY(100%)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+    },
+  },
+  plugins: [],
 }
