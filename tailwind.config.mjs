79245a8836ee24a8e414dc5175112722dd0f0c1d
francisco-leiva <leivafrancisco631@gmail.com>
2024-03-26 18:57:34 -0300

    style: change header style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,16 @@ export default {
     extend: {
       animation: {
         reveal: 'reveal 1s both',
+        menuIn: 'menuIn 150ms both',
       },
       keyframes: {
         reveal: {
           from: { opacity: 0, transform: 'translateY(100%)' },
           to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        menuIn: {
+          from: { opacity: 0, transform: 'scale(0.4)' },
+          to: { opacity: 1, transform: 'scale(1)' },
         },
       },
     },
